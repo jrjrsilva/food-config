23c23f7d6582d3ed4251203ac570a68583b1ee63
Silva <jrjrsilva@gmail.com>
2022-06-11 22:55:00 -0300

    Commit inicial

---
+++ src/test/java/br/com/cjm/solucoes/food/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cjm.solucoes.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
